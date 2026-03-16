0cf13acd0da505a0c8f82cd20bd4ed57e8938605
joaoev <joaoalvesxp@alu.ufc.br>
2026-03-16 11:34:47 -0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-responsive class="border rounded m-4" max-height="300">
+    <v-app>
+      <v-app-bar title="App bar" />
+
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer" />
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-container>
+          <h1 class="my-0">Main Content</h1>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
