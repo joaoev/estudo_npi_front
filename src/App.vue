c0c19bad351ab92c15eea59812366f3b18de4a08
joaoev <joaoalvesxp@alu.ufc.br>
2026-03-18 09:09:50 -0300

    style: habilitando troca de tema

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
     <v-app-bar>
-      <template #prepend>
-        <v-app-bar-nav-icon />
-      </template>
       <v-app-bar-title>Estudo NPI 2026.1</v-app-bar-title>
+      <v-btn icon @click="toggleTheme">
+        <v-icon>{{ theme.global.name.value === 'light' ? 'mdi-moon-waning-crescent' : 'mdi-white-balance-sunny' }}</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -15,4 +15,11 @@
 
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
+  import { useTheme } from 'vuetify'
+
+  const theme = useTheme()
+
+  function toggleTheme () {
+    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+  }
 </script>
